4ffd607d0cfb109ee0c03db34a2d89b6305f14a5
Yamil Burgos <yamil.burgos1293@gmail.com>
2017-12-26 13:03:08 -0500

    Updated Shared Site Layout's CSS & Modified BuyPolicy View

--- HomeInsurance/Models/VerifyPolicy.cs
+++ HomeInsurance/Models/VerifyPolicy.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
-namespace HomeInsurance.Models
-{
+namespace HomeInsurance.Models {
+
     [NotMapped]
-    public class VerifyPolicy
-    {
+    public class VerifyPolicy {
         public int Id { get; set; }
+
+        [Display(Name = "Quote Id")]
         public int QuoteId { get; set; }
+
+        [Display(Name = "Enter Policy Start Date")]
         public string PolicyStartDate { get; set; }
+
         public bool Acknowledge { get; set; }
     }
 }
